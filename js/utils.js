258bedfd49693c3d03fbccf9339472288dff5489
Dahoon Kim <salqueng@gmail.com>
2016-02-14 18:34:30 +0900

    Use slugify template tag to make urls

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,4 @@
 (function (window, anchors) {
-    window.makeHeaderLink = function (selector) {
-        var item_urlify = function (item) {
-            var url_part = anchors.urlify(item.text.trim());
-            item.href += '#' + url_part;
-        };
-        var items = window.document.querySelectorAll(selector);
-        for (var i = 0; i < items.length; i++) {
-            item_urlify(items[i]);
-        }
-    };
     window.domUtils = {
         addClass: function (dom, className) {
             if (!dom) {
